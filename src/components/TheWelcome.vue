bc54142930f3a1a9d63f648f389af22a2d88c07f
CharlesChinedum <charleschinedum2@gmail.com>
2024-02-15 11:41:52 +0100

    initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Welcome To Ignite Vue3 App</template>
+  </WelcomeItem>
+</template>
